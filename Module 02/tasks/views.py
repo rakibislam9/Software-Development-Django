ed5caf48ff9afc61e00b8a137d56b1b6cbc89ef4
Rakib Islam <rakibislam.rh330@gmail.com>
2025-11-25 03:51:38 +0600

    Databaces Canaction

--- Module 02/tasks/views.py
+++ Module 02/tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm
-from tasks.models import Employees
+from tasks.models import Employees, Task
 
 # Create your views here.
 
@@ -30,5 +30,27 @@ def Test(request):
 def task_form(request):
     employees = Employees.objects.all()
     form = TaskForm(employees=employees)
-    context = {"form": form , "employees": employees}
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, employees=employees)
+        if form.is_valid():
+            # print(form.cleaned_data)
+
+            data = form.cleaned_data
+            title = data.get('title')
+            description = data.get('description')
+            due_date = data.get('due_date')
+            assigned_to = data.get('assigned_to')
+
+            task = Task.objects.create(
+                title=title, description=description, due_date=due_date
+            )
+
+            for emp_id in assigned_to:
+                employees = Employees.objects.get(id=emp_id)
+                task.assigned_to.add(employees)
+
+            return HttpResponse("Task Added successfully")
+
+    context = {"form": form}
     return render(request,"dashboard/test_create_mathod.html", context)
