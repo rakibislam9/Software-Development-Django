a71051495f36b1856b89bb8734586633514d1431
Rakib Islam <rakibislam.rh330@gmail.com>
2025-11-25 04:03:12 +0600

    bug fix

--- Module 02/tasks/forms.py
+++ Module 02/tasks/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class TaskForm(forms.Form):
     title = forms.CharField(max_length=250, label="Task Title")
-    descriptton = forms.CharField(widget=forms.Textarea)
+    description = forms.CharField(widget=forms.Textarea)
     due_date = forms.DateField(widget=forms.SelectDateWidget)
     assigned_to = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple, 
